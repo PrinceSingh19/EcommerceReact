9c5415cef76584e680cfbd2bb8dc6c43f2260c1a
Prince Singh <princesingh808087@gmail.com>
2022-12-05 16:59:22 +0530

    tutorial 6 completed

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import styled from "styled-components";
 const Home = () => {
-	return <div>Home</div>;
+	return <Wrapper className="test">Home</Wrapper>;
 };
 
+const Wrapper = styled.section`
+	background-color: ${({ theme }) => theme.colors.bg};
+	height: 100vh;
+`;
 export default Home;
