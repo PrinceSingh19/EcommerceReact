2249d49584eed3619fc74d2c814746d33f60cd3a
Prince Singh <princesingh808087@gmail.com>
2022-12-06 20:20:55 +0530

    tutorial 23 completed

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { useState } from "react";
 import { FaCheck } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { Button } from "../styles/Button";
+import CartAmountToggle from "./CartAmountToggle";
 const AddToCart = ({ product }) => {
 	const { id, stock, colors } = product;
 	const [color, setColor] = useState(colors[0]);
+	const [amount, setAmount] = useState(1);
+	const setDecrease = () => {
+		amount > 1 ? setAmount(amount - 1) : setAmount(1);
+	};
+	const setIncrease = () => {
+		amount < stock ? setAmount(amount + 1) : setAmount(stock);
+	};
 	return (
 		<Wrapper>
 			<div className="colors">
@@ -24,6 +34,11 @@ const AddToCart = ({ product }) => {
 					})}
 				</p>
 			</div>
+			{/*  Add to Cart */}
+			<CartAmountToggle amount={amount} setDecrease={setDecrease} setIncrease={setIncrease} />
+			<NavLink to="/cart">
+				<Button className="btn">Add To Cart</Button>
+			</NavLink>
 		</Wrapper>
 	);
 };
