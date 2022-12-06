be1937275f0edc87e981f3ddd3baa2daaea3846d
Prince Singh <princesingh808087@gmail.com>
2022-12-06 21:29:36 +0530

    tutorial 25 completed

--- src/Products.js
+++ src/Products.js
@@ -3,8 +3,12 @@ import styled from "styled-components";
 import FilterSection from "./components/FilterSection";
 import ProductList from "./components/ProductList";
 import Sort from "./components/Sort";
+import { useFilterContext } from "./context/filter_context";
+import { useProductContext } from "./context/productcontext";
 
 const Products = () => {
+	const { filter_products } = useFilterContext();
+	console.log(filter_products);
 	return (
 		<Wrapper>
 			<div className="container grid grid-filter-column">
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+import { useProductContext } from "../context/productcontext";
+import Product from "./Product";
 
 const ProductList = () => {
-	return <div>ProductList</div>;
+	const { products } = useProductContext();
+
+	return (
+		<div className="grid grid-three-column">
+			{products.map((currElem, index) => {
+				return <Product key={currElem.id} {...currElem} />;
+			})}
+		</div>
+	);
 };
 
 export default ProductList;
