d4d34d21b906d97647515cf75d5fb0852e876d53
Prince Singh <princesingh808087@gmail.com>
2022-12-08 17:40:02 +0530

    tutorial 45 completed

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -3,7 +3,22 @@ import React from "react";
 const cartReducer = (state, action) => {
 	if (action.type === "ADD_TO_CART") {
 		let { id, amount, color, product } = action.payload;
+		const { name, price } = product;
+		let cartProduct = {
+			id: id + color,
+			amount: amount,
+			color: color,
+			name: product.name,
+			price: product.price,
+			image: product.image[0].url,
+			max: product.stock,
+		};
+		return {
+			...state,
+			cart: [...state.cart, cartProduct],
+		};
 	}
+	return state;
 };
 
 export default cartReducer;
