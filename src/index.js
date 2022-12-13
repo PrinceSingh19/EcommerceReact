57a47eb595e91dd2675eca4c98d8bace3c6b552a
Prince Singh <princesingh808087@gmail.com>
2022-12-13 17:04:26 +0530

    removed unnecessary imports from index.js file

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { AppProvider } from "./context/productcontext";
-import { FilterContextProvider } from "./context/filter_context";
-import { CartProvider } from "./context/cart_context";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
